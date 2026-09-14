f910b5b5e2e6b6c42dbf154a311b2d8d591c11a6
eiwashita <erikoiwashita@gmail.com>
2026-09-14 17:08:55 -0600

    fix(posts): tighten spacing for list paragraphs in rich text body

--- components/posts/PostBody.jsx
+++ components/posts/PostBody.jsx
@@ -4,7 +4,7 @@ const PostBody = ({ post }) => {
   const { content } = post.fields;
 
   return (
-    <div className="prose max-w-4xl mx-auto">
+    <div className="prose max-w-4xl mx-auto [&_ul_p]:my-0 [&_ol_p]:my-0 prose-li:my-0">
       <RichText content={content} />
     </div>
   );
